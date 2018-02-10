44af2c4effd933f49799ca0c61245bdcbf246a3f
alicespyglass <alicespyglass@gmail.com>
2018-02-10 19:57:06 +0000

    write functions for edge case all sides the same

--- pancake-flipper.rb
+++ pancake-flipper.rb
@@ -1,26 +1,66 @@
+# s = number of pancakes
+# k = size of flipper
+
 def get_s(line)
-  return line.split[0]
+  return line.split[0].length
 end
 
 def get_k(line)
-  return line.split[1]
+  return line.split[1].to_i
 end
 
-def answer(line, index)
-  s = "solution"
-  puts "Case ##{index}: #{s}"
+def get_s_data(line)
+  return line.split[0]
 end
 
-def question(line, index)
+def count_sides(line, side)
+  data_array = get_s_data(line).split("")
+  count = 0
+  data_array.each do | pancake |
+    if pancake === side
+      count += 1
+    end
+  end
+  return count
+end
+
+def s_equals_k(s, k)
+  return s === k
+end
+
+def all_same_side_up(line)
   s = get_s(line)
+  return count_sides(line, "+") === s || count_sides(line, "-") === s
+end
+
+def answer(line)
+  s = get_s(line)
+  k = get_k(line)
+
+  plus_sides = count_sides(line, "+")
+  minus_sides = count_sides(line, "-")
+
+  if all_same_side_up(line)
+    return "0"
+  end
+  return "..."
+end
+
+def format_answer(line, index)
+  solution = answer(line)
+  puts "Case ##{index}: #{solution}"
+end
+
+def format_question(line, index)
+  s = get_s_data(line)
   k = get_k(line)
   puts "Question ##{index}: #{s} #{k}"
 end
 
 def print(limit, input)
   for i in (1..limit)
-    question(input[i], i)
-    answer(input[i], i)
+    format_question(input[i], i)
+    format_answer(input[i], i)
   end
 end
 
@@ -28,3 +68,5 @@ input = ARGF.readlines
 number_of_tests = input[0].to_i
 
 print(number_of_tests, input)
+
+# count_sides(input[2], "+")
