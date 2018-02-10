69f562f2d6b152d998d7258fd11413088766c13b
alicespyglass <alicespyglass@gmail.com>
2018-02-10 18:56:32 +0000

    update output formatting

--- pancake-flipper.rb
+++ pancake-flipper.rb
@@ -1,9 +1,17 @@
-def test(idx)
-  puts idx*2
+def params(line, index)
+  params = line.split
+  s = params[0]
+  k = params[1]
+  puts "Case ##{index}: #{s} #{k}"
 end
 
-ARGF.each_with_index do |line, idx|
-  if idx > 0
-    print "Case ##{idx}:", test(idx)
+def test(limit, input)
+  for i in (1..limit)
+    params(input[i], i)
   end
 end
+
+lines = ARGF.readlines
+number_of_tests = lines[0].to_i
+
+test(number_of_tests, lines)
