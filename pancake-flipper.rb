831e0515fe09fb216c3068bf6eb63301aac0a3ee
alicespyglass <alicespyglass@gmail.com>
2018-02-10 23:41:42 +0000

    add functions for finding number of consecutive and non-consecutive faces

--- pancake-flipper.rb
+++ pancake-flipper.rb
@@ -33,20 +33,57 @@ def all_same_side_up(line)
   return count_sides(line, "+") === s || count_sides(line, "-") === s
 end
 
+def convert_to_binary(line)
+  array = get_s_data(line).split("")
+
+  return array.map{ |a| a === "-"? a = 0 : a = 1}
+end
+
+def consecutive_sides_up(line)
+  data = get_s_data(line).split("")
+  s = get_s(line) - 2
+  count = 0
+
+  for i in (0..s)
+    if data[i] === data[i + 1]
+      count += 1
+    end
+  end
+  return count
+end
+
+def non_consecutive_sides_up(line)
+  data = get_s_data(line).split("")
+  s = get_s(line) - 2
+  count = 0
+
+  for i in (0..s)
+    if data[i] != data[i + 1]
+      count += 1
+    end
+  end
+  return count
+end
+
+
 def answer(line)
   s = get_s(line)
   k = get_k(line)
+  n = non_consecutive_sides_up(line)
 
   plus_sides = count_sides(line, "+")
   minus_sides = count_sides(line, "-")
 
   if all_same_side_up(line)
     return "0"
-  end
-  if s === k && !all_same_side_up(line)
+  elsif s_equals_k(s,k) && !all_same_side_up(line)
     return "IMPOSSIBLE"
+  elsif k > s/2 && n > 1
+    return "IMPOSSIBLE"
+  elsif (consecutive_sides_up(line) === k - 1) && (non_consecutive_sides_up(line) === 2)
+    return "CHECK"
+  else return "..."
   end
-  return "..."
 end
 
 def format_answer(line, index)
@@ -79,6 +116,7 @@ end
 input = ARGF.readlines
 number_of_tests = input[0].to_i
 
+consecutive_sides_up(input[3])
+non_consecutive_sides_up(input[3])
 print(number_of_tests, input)
-
 # count_sides(input[2], "+")
